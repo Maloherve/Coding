df70e88d8a8ffec78c2f9734e1f4829cbe612e70
Malo Herve <malo.herve@epfl.ch>
2023-01-14 11:39:24 +0100

    training with modifying db

--- DiscordBot/CthulhuBot.js
+++ DiscordBot/CthulhuBot.js
@@ -1,8 +1,18 @@
 import { Client, Events, GatewayIntentBits } from 'discord.js';
 import fetch from "node-fetch"
 import config from './config.json' assert {type: "json"};
+import database from './database.json' assert {type: "json"};
+
 
 const client = new Client({intents: 3276799})
+
+const sadWords = ["sad", "depressed", "unhappy", "angry"]
+const encouragements = [
+    "Cheer Up",
+    "Hang in there", 
+    "You are a great person / bot"
+]
+
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
@@ -26,8 +36,15 @@ function getQuote() {
 client.on('messageCreate', msg => {
     if (msg.author.bot) return
 
+
     if (msg.content === "$inspire") {
         getQuote().then(quote => msg.channel.send(quote))
+    }
+
+
+    if (sadWords.some( word => msg.content.includes(word) )){ // This goes through every item in sadWords, and applies function to it
+        const encouragement = encouragements[Math.floor(Math.random() * encouragements.length)]
+        msg.reply(encouragement)
     }
 })
 
@@ -37,12 +54,10 @@ client.on('messageCreate', msg => {
 
 
 
-client.login(config.TOKEN)
 
 
 
-// const channel = client.channels.cache.get()
-// channel.send('content');
 
+console.log(database.sad_words)
 
-
+// client.login(config.TOKEN)
--- DiscordBot/test.js
+++ DiscordBot/test.js
@@ -1,3 +1,56 @@
-import data from './config.json' assert {type: "json"};
+import fs from 'fs'
+// import data from './database.json' assert {type: "json"};
 
-console.log(data.token)
+
+let data = fs.readFileSync('DiscordBot/database.json')
+var myObject = JSON.parse(data)[0]
+
+// let newData = {
+//     HappyWords: ["TUT", "SNOOT", "BLOOP"] 
+// }
+
+// myObject.push(newData)
+
+// var newObject = JSON.stringify(myObject)
+// console.log(newObject)
+
+// fs.writeFile('DiscordBot/database.json', newObject, err => {
+//     if (err) {throw err}
+//     console.log("New Data added") 
+// });
+
+
+function modifyDatabaseEntry(key, type, value){
+    let file = fs.readFileSync('DiscordBot/database.json');
+    var myObject = JSON.parse(file);
+
+    let modify;
+    switch (type) {
+        case 'add':
+            modify = (obj) => obj[0][key] = obj[0][key].concat(value);
+            break;
+        case 'modify':
+            modify = (obj) => obj[0][key] = value;
+            break;
+        default:
+            console.log('Invalid modify type. Please use "add" or "modify".')
+    }
+
+    modify(myObject) 
+
+    let newObject = JSON.stringify(myObject, null, 3);
+
+    console.log(newObject)
+
+
+    fs.writeFile('DiscordBot/database.json', newObject, err => { 
+        if (err) {throw err;}
+        console.log("Modifications were made.")
+    })
+}
+
+
+
+
+modifyDatabaseEntry("SadWords", "add", "woop")
+
