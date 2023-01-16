9284e69d23576d58b997b5238d447ca337dc1378
Malo <malo.herve1@gmail.com>
2023-01-16 17:22:15 +0100

    Updated bot to not use fetch, instead use require

--- DiscordBot/CthulhuBot.js
+++ DiscordBot/CthulhuBot.js
@@ -1,19 +1,14 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js';
-import fetch from "node-fetch"
-import config from './config.json' assert {type: "json"};
-import database from './database.json' assert {type: "json"};
+const { Client, Events, GatewayIntentBits } = require( 'discord.js' );
+const config = require( './token_bot.json' );
+const database = require( './database.json' );
 
 
+// Load Client with all authorizations
 const client = new Client({intents: 3276799})
 
-const sadWords = ["sad", "depressed", "unhappy", "angry"]
-const encouragements = [
-    "Cheer Up",
-    "Hang in there", 
-    "You are a great person / bot"
-]
 
 
+// When Bot Connects
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
 })
@@ -21,43 +16,17 @@ client.on("ready", () => {
 
 
 
-function getQuote() {
-    return fetch("https://zenquotes.io/api/random")
-    .then(res => {    // Convert to json
-        return res.json();
-    })
-    .then(data => {
-        return data[0]["q"] + " -" + data[0]["a"]  // Format in appropriate way
-    })
-}
-
-
 
 client.on('messageCreate', msg => {
+    // Don't react to own message
     if (msg.author.bot) return
 
 
-    if (msg.content === "$inspire") {
-        getQuote().then(quote => msg.channel.send(quote))
-    }
-
-
-    if (sadWords.some( word => msg.content.includes(word) )){ // This goes through every item in sadWords, and applies function to it
-        const encouragement = encouragements[Math.floor(Math.random() * encouragements.length)]
-        msg.reply(encouragement)
-    }
+    if (msg.content === "$inspire") { msg.reply("This is not a functionality anymore.") }
 })
 
 
 
 
 
-
-
-
-
-
-
-console.log(database.sad_words)
-
-// client.login(config.TOKEN)
+client.login(config.TOKEN)
